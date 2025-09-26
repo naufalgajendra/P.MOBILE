1dc7e938c8cea65bd50a544767aa6e7687dc2046
NaufalGajendra <naufal.gajendra@gmail.com>
2025-09-26 10:56:18 +0700

    Add files via upload

---
+++ dart_example1/bin/dart_example1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_example1/dart_example1.dart' as dart_example1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_example1.calculate()}!');
+}
---
+++ dart_example1/test/dart_example1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_example1/dart_example1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
